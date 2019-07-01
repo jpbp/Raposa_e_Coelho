c1b582f25b9e97ee810f3e9e6d48ee335ba65c9a
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2019-06-30 21:07:03 -0300

    encerrar execução ao fecha janela implementado

--- Raposa_E_Coelho/src/raposa_e_coelho/Principal.java
+++ Raposa_E_Coelho/src/raposa_e_coelho/Principal.java
@@ -2,6 +2,8 @@ package raposa_e_coelho;
 public class Principal{
   public static void main(String[] args) throws InterruptedException{
     Simulator simulator = new Simulator();
-    simulator.runLongSimulation();
+      for (int i = 0; i < 10; i++) {
+          simulator.simulateOneStep(); 
+      }
   }
 }
