722bb5a0f888433be0bbdb3ef974c0180938c124
luizc <luizc@DESKTOP-3SLMIT1>
2019-07-01 11:02:31 -0300

    Grama e fome no coelho implementada

--- Raposa_E_Coelho/src/raposa_e_coelho/Principal.java
+++ Raposa_E_Coelho/src/raposa_e_coelho/Principal.java
@@ -2,8 +2,7 @@ package raposa_e_coelho;
 public class Principal{
   public static void main(String[] args) throws InterruptedException{
     Simulator simulator = new Simulator();
-      for (int i = 0; i < 10; i++) {
-          simulator.simulateOneStep(); 
+       simulator.runLongSimulation();
       }
   }
-}
+
