8e511c29f76df6e9bdd62cd974edefc0c0612659
LuizCalvo <50885734+LuizCalvo@users.noreply.github.com>
2019-07-04 19:57:22 -0300

    Javadoc Principal

--- Raposa_E_Coelho/src/raposa_e_coelho/Principal.java
+++ Raposa_E_Coelho/src/raposa_e_coelho/Principal.java
@@ -1,6 +1,17 @@
 package raposa_e_coelho;
+/**
+ * A classe Principal, que contém o método Main.
+ * @author João Paulo Pena, Luiz Felipe Calvo, Raphael Fernandes Roriz.
+ */
 public class Principal{
+  
+  /**
+ * O método main da simulação, responsável por iniciar a simulação propriamente dita,e
+ * definir quantos Steps serão rodados
+ * @param argumentos da linha de comando
+ */
   public static void main(String[] args) throws InterruptedException{
+    //Roda a simulação.
     Simulator simulator = new Simulator();
        simulator.runLongSimulation();
       }
